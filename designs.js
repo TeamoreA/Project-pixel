25f13a3ec1550bc3000324b1fd296a711bdfd028
Teamo <teamorekamau97@gmail.com>
2018-06-30 22:08:51 +0300

    class inputWidth fixed

--- designs.js
+++ designs.js
@@ -3,18 +3,19 @@
 
 // When size is submitted by the user, call makeGrid()
 var height, width, color;
-
+// This reads the user input(grid with and height)
 $('#sizePicker').submit(function (event) {
 	
 	height = $('#inputHeight').val();
-	width = $('#inputHeight').val();
+	width = $('#inputwidth').val();
 	makeGrid(height, width);
 	event.preventDefault();
 });
 
+// makeGrid function with two parameter to generate the grid
 function makeGrid(rows, cols) {
 
-// Your code goes here!
+
 	$('tr').remove();
 
 	for (var i = 1; i <= rows; i++) {
@@ -24,7 +25,7 @@ function makeGrid(rows, cols) {
 		}
 	}
 
-
+	// This colors the grid
 	$('td').click(function() {
 		color = $('#colorPicker').val();
 		$(this).attr('style') ? $(this).removeAttr('style') : $(this).attr('style', 'background-color:'+color);
